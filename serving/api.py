7caa9cedafd070dd77a49899fd0c0c5a995f0bbe
kaelynn-rose <rose.kaelynn@gmail.com>
2025-01-29 12:43:53 -0800

    Added api.py and conf.py

--- serving/api.py
+++ serving/api.py
@@ -16,3 +16,12 @@ app = FastAPI(
     description = 'TODO',
     version=VERSION
 )
+
+@app.get('/earthquake-detection/', response_model=conf.HealthCheck)
+@app.get('/earthquake-detection', response_model=conf.HealthCheck, include_in_schema=False)
+async def health_check():
+    """Surfaces the result of the backend tensorflow serving model status check.
+    This tensorflow model is the artifact that provides predictions."""
+    return {
+        'model_version_status'
+    }
